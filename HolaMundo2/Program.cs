9f544b0f32c69bd4897981fd24fb9051f2b50ba2
josedavid-quero <josedavid.quero@iescampanillas.com>
2022-10-04 14:16:47 +0200

    Primer cambio

--- HolaMundo2/Program.cs
+++ HolaMundo2/Program.cs
@@ -10,7 +10,7 @@ namespace HolaMundo2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("¡Hola mundo!");
+            Console.WriteLine("¡¡¡Hola mundo!!!");
         }
     }
 }
